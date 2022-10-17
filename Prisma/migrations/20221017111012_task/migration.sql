fbc81c117b4b4c996a36793c30241ac39034fd0a
Michele M <mnmin@gmail.com>
2022-10-17 12:10:52 +0100

    Prisma Schema Task

---
+++ Prisma/migrations/20221017111012_task/migration.sql
@@ -0,0 +1,18 @@
+-- CreateTable
+CREATE TABLE "Task" (
+    "id" SERIAL NOT NULL,
+    "taskName" TEXT NOT NULL,
+    "taskDescription" TEXT NOT NULL,
+    "linksUrl" VARCHAR(255),
+    "userId" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Task_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Task_userId_key" ON "Task"("userId");
+
+-- AddForeignKey
+ALTER TABLE "Task" ADD CONSTRAINT "Task_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
